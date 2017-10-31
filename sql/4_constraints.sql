92d78efc3eeb41e4292d0fd5d0aca47c1fc4838d
Michael Stocker <mic.sto@hotmail.com>
2017-10-31 13:40:53 +0100

    added alter table statements for adding foreign keys

---
+++ sql/4_constraints.sql
@@ -0,0 +1,23 @@
+/*
+ * CONSTRAINTS 'CocktailmixerDB'
+ *
+ * Stocker Michael
+ * Zollinger Marco
+ */
+
+ALTER TABLE cocktail_rezept
+ADD CONSTRAINT fk_glastyp_von_glastyp_name
+    FOREIGN KEY (glas_typ) REFERENCES glas_typ (name)
+;
+
+ALTER TABLE zutaten_zuteilung
+ADD CONSTRAINT fk_cocktailrezept_von_cocktailrezept_name
+    FOREIGN KEY (cocktail_rezept) REFERENCES cocktail_rezept (name),
+    CONSTRAINT fk_zutaten_von_zutaten_name
+    FOREIGN KEY (zutaten) REFERENCES zutaten (name)
+;
+
+ALTER TABLE festzutaten
+ADD CONSTRAINT fk_name_von_zutaten_name
+    FOREIGN KEY (name) REFERENCES zutaten (name)
+;
