27fe52c1f8915535a8a8a9cfef135658312877fb
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-10-31 17:48:28 +0100

    added rating constraint

--- sql/4_constraints.sql
+++ sql/4_constraints.sql
@@ -12,12 +12,22 @@ ADD CONSTRAINT fk_glastyp_von_glastyp_name
 
 ALTER TABLE zutaten_zuteilung
 ADD CONSTRAINT fk_cocktailrezept_von_cocktailrezept_name
-    FOREIGN KEY (cocktail_rezept) REFERENCES cocktail_rezept (name),
-    CONSTRAINT fk_zutaten_von_zutaten_name
+    FOREIGN KEY (cocktail_rezept) REFERENCES cocktail_rezept (name)
+;
+
+ALTER TABLE zutaten_zuteilung
+ADD CONSTRAINT fk_zutaten_von_zutaten_name
     FOREIGN KEY (zutaten) REFERENCES zutaten (name)
 ;
 
 ALTER TABLE festzutaten
 ADD CONSTRAINT fk_name_von_zutaten_name
     FOREIGN KEY (name) REFERENCES zutaten (name)
+;
+
+ALTER TABLE cocktail_rezept
+ADD rating INTEGER;
+
+ALTER TABLE cocktail_rezept
+ALTER COLUMN rating SET DEFAULT 5
 ;
