fb7ad4d717eea6715f833e9a428604410902315f
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-10-31 15:29:50 +0100

    created cuba libre test insert

--- sql/3_inserts.sql
+++ sql/3_inserts.sql
@@ -9,16 +9,21 @@
 -- Glasvolumen: Volumen_ml = PI * (Durchmesser_cm/2)^2 * Hoehe_cm
 
 -- TABLE COCKTAILREZEPT
-INSERT INTO cocktail_rezept VALUES ();
+INSERT INTO cocktail_rezept VALUES ('Cuba Libre', 'Highball', TRUE);
 
 -- TABLE GLASTYP
-INSERT INTO glas_typ VALUES ();
+INSERT INTO glas_typ VALUES ('Highball', 3, 269, 7, 15);
 
 -- TABLE ZUTATEN
-INSERT INTO zutaten VALUES ();
+INSERT INTO zutaten VALUES ('Cola', 1500, 0);
+INSERT INTO zutaten VALUES ('Rum hell', 750, 42);
+INSERT INTO zutaten VALUES ('Limettensaft', 150, 0);
 
 -- TABLE ZUTATENZUTEILUNG
-INSERT INTO zutaten_zuteilung VALUES ();
+INSERT INTO zutaten_zuteilung VALUES ('Cuba Libre', 'Cola', 120);
+INSERT INTO zutaten_zuteilung VALUES ('Cuba Libre', 'Rum hell', 50);
+INSERT INTO zutaten_zuteilung VALUES ('Cuba Libre', 'Limettensaft', 10);
+INSERT INTO zutaten_zuteilung VALUES ('Cuba Libre', 'Eiswuerfel', NULL);    -- FIXME
 
 -- TABLE FESTZUTATEN
-INSERT INTO festzutaten VALUES ();
+INSERT INTO festzutaten VALUES ('Eiswuerfel', 1, 14, 10);
