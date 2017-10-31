28523c9a0b6b0c2b4d55c97f355831aec38b70fe
Michael Stocker <mic.sto@hotmail.com>
2017-10-31 11:13:32 +0100

    added sql schema

---
+++ sql/2_schema.sql
@@ -0,0 +1,39 @@
+/*
+ * DATABASE SCHEMA 'CocktailmixerDB'
+ *
+ * Stocker Michael
+ * Zollinger Marco
+ */
+
+CREATE TABLE cocktail_rezept (
+    name             TEXT    PRIMARY KEY,
+	glas_typ         TEXT    NOT NULL,
+	enthaelt_alkohol BOOLEAN DEFAULT TRUE
+)
+
+CREATE TABLE glas_typ (
+    name             TEXT    PRIMARY KEY,
+	anzahl           INTEGER DEFAULT 0,
+	volumen_ml       INTEGER NOT NULL,
+	durchmesser_mm   INTEGER NOT NULL,
+	hoehe_mm         INTEGER NOT NULL
+)
+
+CREATE TABLE zutaten (
+    name             TEXT    PRIMARY KEY,
+	vorrat_ml        INTEGER DEFAULT 0,
+	alkohol_volproz  INTEGER NOT NULL
+)
+
+CREATE TABLE zutaten_zuteilung (
+    cocktail_rezept  TEXT    NOT NULL,
+	zutaten          TEXT    NOT NULL,
+	volumen_ml       INTEGER NOT NULL
+)
+
+CREATE TABLE festzutaten (
+    name             TEXT    PRIMARY KEY,
+	dichte_g_pro_cm3 INTEGER NOT NULL,
+	durchmesser_mm   INTEGER NOT NULL,
+	hoehe_mm         INTEGER NOT NULL
+)
