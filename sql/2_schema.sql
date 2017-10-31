7953a39adfc3981d9424db827722e64d5526727d
Michael Stocker <mic.sto@hotmail.com>
2017-10-31 13:43:48 +0100

    bugfix semicolons missing from statements

--- sql/2_schema.sql
+++ sql/2_schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE cocktail_rezept (
     name                TEXT    PRIMARY KEY,
     glas_typ            TEXT    NOT NULL,
     enthaelt_alkohol    BOOLEAN DEFAULT TRUE
-)
+);
 
 CREATE TABLE glas_typ (
     name                TEXT    PRIMARY KEY,
@@ -17,23 +17,23 @@ CREATE TABLE glas_typ (
     volumen_ml          INTEGER NOT NULL,
     durchmesser_cm      INTEGER NOT NULL,
     hoehe_cm            INTEGER NOT NULL
-)
+);
 
 CREATE TABLE zutaten (
     name                TEXT    PRIMARY KEY,
     vorrat_ml           INTEGER DEFAULT 0,
     alkohol_volproz     INTEGER NOT NULL
-)
+);
 
 CREATE TABLE zutaten_zuteilung (
     cocktail_rezept     TEXT    PRIMARY KEY,
     zutaten             TEXT    PRIMARY KEY,
     volumen_ml          INTEGER NOT NULL
-)
+);
 
 CREATE TABLE festzutaten (
     name                TEXT    PRIMARY KEY,
     dichte_g_pro_cm3    INTEGER NOT NULL,
     durchmesser_mm      INTEGER NOT NULL,
     hoehe_mm            INTEGER NOT NULL
-)
+);
