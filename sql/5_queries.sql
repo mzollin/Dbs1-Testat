4e1c7e2940d7f78f0b8d4b2e241759adc5052e15
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-11-21 18:10:17 +0100

    improved aliases

--- sql/5_queries.sql
+++ sql/5_queries.sql
@@ -6,20 +6,22 @@
  */
 
 -- SELECT zutaten and present them nicely
-SELECT cocktail_rezept,
-       zutaten as zutaten_name,
-       volumen_ml as menge,
-       alkohol_volproz as alkoholgehalt,
+-- TODO: improve this query and description
+SELECT cocktail_rezept as CocktailRezept,
+       zutaten as ZutatenName,
+       volumen_ml as Menge,
+       alkohol_volproz as Alkoholgehalt,
        EXISTS(SELECT * FROM festzutaten WHERE "name" = zutaten_zuteilung.zutaten) as fest
   FROM zutaten_zuteilung FULL OUTER JOIN zutaten
   ON zutaten_zuteilung.zutaten = zutaten."name";
 
 -- Get a list of glass types needed for the known cocktail recipes
-SELECT DISTINCT glas_typ FROM cocktail_rezept;
+SELECT DISTINCT glas_typ as GlasTyp
+  FROM cocktail_rezept;
 
 -- Count how many recipes exist for each glass type, order descending by occurence
 SELECT COUNT(name),
-       glas_typ
+       glas_typ as GlasTyp
   FROM cocktail_rezept
   GROUP BY glas_typ
   ORDER BY COUNT(name) DESC;
