5f0f04f11c4e6da778833ae1e00909dbeb9e0cfb
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-11-21 17:41:30 +0100

    added DISTINCT query

--- sql/5_queries.sql
+++ sql/5_queries.sql
@@ -6,4 +6,13 @@
  */
 
 -- SELECT zutaten and present them nicely
-SELECT cocktail_rezept, zutaten as zutaten_name, volumen_ml as menge, alkohol_volproz as alkoholgehalt, EXISTS(SELECT * FROM festzutaten WHERE "name" = zutaten_zuteilung.zutaten) as fest FROM zutaten_zuteilung FULL OUTER JOIN zutaten ON zutaten_zuteilung.zutaten = zutaten."name";
+SELECT cocktail_rezept,
+       zutaten as zutaten_name,
+       volumen_ml as menge,
+       alkohol_volproz as alkoholgehalt,
+       EXISTS(SELECT * FROM festzutaten WHERE "name" = zutaten_zuteilung.zutaten) as fest
+  FROM zutaten_zuteilung FULL OUTER JOIN zutaten
+  ON zutaten_zuteilung.zutaten = zutaten."name";
+
+-- Get a list of glass types needed for the known cocktail recipes
+SELECT DISTINCT glas_typ FROM cocktail_rezept;
