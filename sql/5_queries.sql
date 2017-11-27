ee8ddb5546a57662f1f69801f4f3313d69cadd6c
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-11-27 22:32:32 +0100

    fixed view 3.2 update (6th try)

--- sql/5_queries.sql
+++ sql/5_queries.sql
@@ -176,7 +176,7 @@ CREATE OR REPLACE VIEW festzutatenliste AS
     FROM zutaten
     INNER JOIN festzutaten ON zutaten.name = festzutaten.name;
 
-UPDATE festzutatenliste SET Festzutaten = 'Crushed Eis' WHERE Festzutaten = 'Eiswuerfel';
+UPDATE festzutatenliste SET zutaten.vorrat_ml = '36' WHERE Festzutaten = 'Eiswuerfel';
 --UPDATE festzutatenliste SET zutaten.name = 'Crushed Eis' WHERE zutaten.name = 'Eiswuerfel';
 
 -- view query
