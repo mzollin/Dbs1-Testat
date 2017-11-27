0daddba1c7decad77760c2d1ca96f520a1abb85d
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-11-27 23:03:28 +0100

    added quotes to aliases

--- sql/5_queries.sql
+++ sql/5_queries.sql
@@ -6,28 +6,28 @@
  */
 
 -- 1.1: Get a list of glass types needed for the known cocktail recipes
-SELECT DISTINCT glas_typ AS GlasTyp
+SELECT DISTINCT glas_typ AS "GlasTyp"
   FROM cocktail_rezept;
   
 -- 1.2: Show all cocktail recipes, the corresponding glasses, their volumes, and how much the glasses are filled
-SELECT cocktail_rezept."name" AS CocktailRezept,
-       glas_typ."name" AS GlasTyp,
-       glas_typ.volumen_ml AS GlasVolumen,
-       SUM(zutaten_zuteilung.volumen_ml) AS CocktailVolumen,
-       (SUM(zutaten_zuteilung.volumen_ml)::decimal / glas_typ.volumen_ml * 100)::int AS ProzentGefuellt
+SELECT cocktail_rezept."name" AS "CocktailRezept",
+       glas_typ."name" AS "GlasTyp",
+       glas_typ.volumen_ml AS "GlasVolumen",
+       SUM(zutaten_zuteilung.volumen_ml) AS "CocktailVolumen",
+       (SUM(zutaten_zuteilung.volumen_ml)::decimal / glas_typ.volumen_ml * 100)::int AS "ProzentGefuellt"
   FROM cocktail_rezept
   INNER JOIN glas_typ ON cocktail_rezept.glas_typ = glas_typ."name"
   INNER JOIN zutaten_zuteilung ON cocktail_rezept."name" = zutaten_zuteilung.cocktail_rezept
   GROUP BY cocktail_rezept."name", glas_typ."name", glas_typ.volumen_ml;
   
 -- 1.3: Show all cocktail recipes that contain light or dark rum, and if they contain alcohol
-SELECT "name" AS CocktailRezept, enthaelt_alkohol AS Alkoholgehalt
+SELECT "name" AS "CocktailRezept", enthaelt_alkohol AS "Alkoholgehalt"
   FROM cocktail_rezept
   WHERE "name" IN (SELECT cocktail_rezept FROM zutaten_zuteilung WHERE zutaten IN ('Rum hell', 'Rum dunkel'));
 
 -- 1.4: Count how many recipes exist for each glass type, order descending by occurence
 SELECT COUNT("name"),
-       glas_typ AS GlasTyp
+       glas_typ AS "GlasTyp"
   FROM cocktail_rezept
   GROUP BY glas_typ
   ORDER BY COUNT("name") DESC;
