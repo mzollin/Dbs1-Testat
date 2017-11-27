cc17196cd8185e2cac086730c3c42486a6dc0a9e
Marco Zollinger <marco_zollinger_17@hotmail.com>
2017-11-27 14:46:18 +0100

    added placeholders for exercises

--- sql/5_queries.sql
+++ sql/5_queries.sql
@@ -7,24 +7,39 @@
 
 -- SELECT zutaten and present them nicely
 -- TODO: improve this query and description
-SELECT cocktail_rezept as CocktailRezept,
-       zutaten as ZutatenName,
-       volumen_ml as Menge,
-       alkohol_volproz as Alkoholgehalt,
-       EXISTS(SELECT * FROM festzutaten WHERE "name" = zutaten_zuteilung.zutaten) as fest
+SELECT cocktail_rezept AS CocktailRezept,
+       zutaten AS ZutatenName,
+       volumen_ml AS Menge,
+       alkohol_volproz AS Alkoholgehalt,
+       EXISTS(SELECT * FROM festzutaten WHERE "name" = zutaten_zuteilung.zutaten) AS fest
   FROM zutaten_zuteilung FULL OUTER JOIN zutaten
   ON zutaten_zuteilung.zutaten = zutaten."name";
 
--- Get a list of glass types needed for the known cocktail recipes
-SELECT DISTINCT glas_typ as GlasTyp
+-- 1.1: Get a list of glass types needed for the known cocktail recipes
+SELECT DISTINCT glas_typ AS GlasTyp
   FROM cocktail_rezept;
+  
+-- 1.2: 
+SELECT cocktail_rezept.name, glas_typ.name, glas_typ.volumen_ml
+  FROM cocktail_rezept INNER JOIN glas_typ ON cocktail_rezept.glas_typ = glas_typ.name;
 
--- Count how many recipes exist for each glass type, order descending by occurence
+-- 1.3: 
+
+-- 1.4: Count how many recipes exist for each glass type, order descending by occurence
 SELECT COUNT(name),
-       glas_typ as GlasTyp
+       glas_typ AS GlasTyp
   FROM cocktail_rezept
   GROUP BY glas_typ
   ORDER BY COUNT(name) DESC;
   
-SELECT cocktail_rezept.name, glas_typ.name, glas_typ.volumen_ml
-  FROM cocktail_rezept INNER JOIN glas_typ ON cocktail_rezept.glas_typ = glas_typ.name;
+-- 1.5: 
+
+  
+-- 2.1: 
+
+-- 2.2: 
+
+
+-- 3.1: 
+
+-- 3.2: 
